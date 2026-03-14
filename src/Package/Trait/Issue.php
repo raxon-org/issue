4372db68b069846e7a21a040214411869fb47a5c
Remco van der Velde <remco@universeorange.com>
2026-03-14 15:33:37 +0100

    issue create

--- src/Package/Trait/Issue.php
+++ src/Package/Trait/Issue.php
@@ -1,16 +1,34 @@
 <?php
 namespace Package\Raxon\Issue\Trait;
 
+use Exception;
+use Raxon\Exception\ObjectException;
+use Raxon\Node\Module\Node;
+
 trait Issue {
     const NAME = 'Issue';
 
+    /**
+     * @throws ObjectException
+     * @throws Exception
+     */
     public function create($flags, $options): void
     {
         $object = $this->object();
-        d($options);
-        //user uuid
-        //title
-        //description
+        $node = new Node($object);
+        $class = 'Application.Issue';
+        $role = $node->role_system();
+
+        $time = microtime(true);
+
+        $create = [];
+        $create['user'] = $options->user->uuid ?? null;
+        $create['title'] = $options->title ?? null;
+        $create['description'] = $options->description ?? [];
+        $create['is']['created'] = $time;
+        $create['is']['modified'] = $time;
+        $response = $node->create($class, $role, $create, $options);
+        d($response);
     }
 
     public function update($flags, $options): void
