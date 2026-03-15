f334eb094bf0ec1a07ef269e6d2c85bc268adcb5
Remco van der Velde <remco@universeorange.com>
2026-03-15 15:23:44 +0100

    issue create

---
+++ src/Package/Output/Filter/Application/Issue.php
@@ -0,0 +1,46 @@
+<?php
+namespace Package\Raxon\Issue\Output\Filter\Application;
+
+use Raxon\App;
+
+use Raxon\Module\Controller;
+
+class Issue extends Controller {
+    const DIR = __DIR__ . '/';
+
+    public static function comment(App $object, $response=null): array | object
+    {
+        $result = [];
+        $count = 0;
+        ddd($response);
+        if(
+            !empty($response) &&
+            (
+                is_object($response) ||
+                is_array($response)
+            )
+        ) {
+            foreach ($response as $nr => $record) {
+                if (
+                    is_array($record) &&
+                    array_key_exists('name', $record)
+                ) {
+                    $result[$record['name']] = $record;
+                    $count++;
+                } elseif (
+                    is_object($record) &&
+                    property_exists($record, 'name')
+                ) {
+                    $result[$record->name] = $record;
+                    $count++;
+                }
+            }
+        }
+        if($count > 0){
+            return (object) $result;
+        } else {
+            return $response;
+        }
+
+    }
+}
