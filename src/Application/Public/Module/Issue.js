eea5136e380b027329df7e8d272e0d5ebd81cf59
Remco van der Velde <remco@universeorange.com>
2026-03-27 15:27:36 +0100

    init

--- src/Application/Public/Module/Issue.js
+++ src/Application/Public/Module/Issue.js
@@ -248,10 +248,7 @@ issue.sleep = (ms) => {
 
 issue.rgb_to_rgba = (rgb, alpha) => {
     rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-    function hex(x) {
-        return ("0" + parseInt(x).toString(16)).slice(-2);
-    }
-    return "rgba(" + hex(rgb[1]) + "," + hex(rgb[2]) + "," + hex(rgb[3]) + "," + alpha + ")";
+    return "rgba(" + rgb[1] + "," + rgb[2] + "," + rgb[3] + "," + alpha + ")";
 }
 
 issue.list = async (id) => {
