b7536934ec905353f6faae29bdeaf432f5a8cbf5
Remco van der Velde <remco@universeorange.com>
2026-03-29 19:03:50 +0200

    init

--- src/Application/Public/Module/Issue.js
+++ src/Application/Public/Module/Issue.js
@@ -618,11 +618,11 @@ issue.new = (id) => {
     menu_application_issue.select('li.active').removeClass('active');
     let button_new = section.select('.menu-application-issue li[data-tab="issue-new"]');
     button_new.addClass('active');
-    section.select('.body .issue-list').addClass('display-none');
-    section.select('.body .issue-list .checkbox-status').addClass('display-none');
-    section.select('.body .issue-new').removeClass('display-none');
-    section.select('.footer .issue-list').addClass('display-none');
-    section.select('.footer .issue-new').removeClass('display-none');
+    section.select('.body .issue-list')?.addClass('display-none');
+    section.select('.footer .checkbox-status')?.addClass('display-none');
+    section.select('.body .issue-new')?.removeClass('display-none');
+    section.select('.footer .issue-list')?.addClass('display-none');
+    section.select('.footer .issue-new')?.removeClass('display-none');
     let issue = section.select('.body .issue-new');
     if(issue){
         let title = section.select('.body .issue-new input[name="title"]');
